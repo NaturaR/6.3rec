5a3cd6ff5dbba12eee0cf9d7a711b2ab9cca1b6a
Roman 111 <romanvydysh7@gmail.com>
2023-11-27 16:35:20 +0200

    First

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void printArray(int* arr, int size, int index = 0) {
+    if (index < size) {
+        cout << setw(4) << arr[index];
+        printArray(arr, size, index + 1);
+    }
+}
+
+void fillArray(int* arr, int size, int index = 0) {
+    if (index < size) {
+        cout << "a[" << index << "] = ";
+        cin >> arr[index];
+        fillArray(arr, size, index + 1);
+    }
+}
+
+bool isNonDecreasingRecursive(int arr[], int size, int index = 0) {
+    if (index < size - 1) {
+        if (arr[index] > arr[index + 1]) {
+            return false;
+        }
+        return isNonDecreasingRecursive(arr, size, index + 1);
+    }
+    return true;
+}
+
+int main() {
+    const int size = 5;
+    int arr[size];
+
+    fillArray(arr, size);
+    printArray(arr, size);
+
+    if (isNonDecreasingRecursive(arr, size)) {
+        cout << '\n';
+        cout << "True" << endl;
+    }
+    else {
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
---
+++ ConsoleApplication1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+        TEST_METHOD(TestIsNonDecreasingRecursive)
+        {
+            const int size = 5;
+            int arr1[size] = { 1, 2, 3, 4, 5 };
+            Assert::IsTrue(isNonDecreasingRecursive(arr1, size));
+
+            int arr2[size] = { 5, 4, 3, 2, 1 };
+            Assert::IsFalse(isNonDecreasingRecursive(arr2, size));
+        }
+	};
+}
